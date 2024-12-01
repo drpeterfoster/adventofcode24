bb1758f2c718df2652d97000fdc2d575f6487d60
drpeterfoster <peterfoster@gmail.com>
2024-12-01 00:57:17 -0700

    fixes

--- 00template.py
+++ 00template.py
@@ -5,43 +5,40 @@ import pandas as pd
 from aocd.models import Puzzle
 
 
-def tester(func, example):
-    assert func(example.input_data) == (example.answer_a if example.answer_a is not None else example.answer_b)
-    print("tests pass")
-    return True
-
-
 # %%
-puz = Puzzle(year=2023, day=1)
+puz = Puzzle(year=2024, day=1)
 puz.view()
-print(puz.input_data)
 
 
 # %%
-def parse_data(data):
+def parse_data(data: str):
     pass
 
 
 def part1(data=None):
-    result = None
+    x = parse_data(data)
+    result = 0
     print(result)
     return result
 
 
 def part2(data=None):
-    result = None
+    x = parse_data(data)
+    result = 0
     print(result)
     return result
 
 
 # %%
-if tester(part1, puz.examples[0]):
-    resa = part1(puz.input_data)
-    print(f"solution: {resa}")
-    puz.answer_a = resa
+print("answer:", puz.examples[0].answer_a)
+# resa = part1(puz.input_data)
+# print(f"solution: {resa}")
+# puz.answer_a = resa
 
 # %%
-if tester(part2, puz.examples[1]):
-    resb = part2(puz.input_data)
-    print(f"solution: {resb}")
-    puz.answer_b = resb
+print("answer:", puz.examples[0].answer_b)
+# resb = part2(puz.input_data)
+# print(f"solution: {resb}")
+# puz.answer_b = resb
+
+# %%
--- day1.py
+++ day1.py
@@ -5,43 +5,55 @@ import pandas as pd
 from aocd.models import Puzzle
 
 
-def tester(func, example):
-    assert func(example.input_data) == (example.answer_a if example.answer_a is not None else example.answer_b)
-    print("tests pass")
-    return True
-
-
 # %%
 puz = Puzzle(year=2024, day=1)
 puz.view()
-print(puz.input_data)
 
 
 # %%
-def parse_data(data):
-    pass
+def parse_data(data: str):
+    list1 = []
+    list2 = []
+
+    lines = data.strip().split("\n")
+    for line in lines:
+        num1, num2 = map(int, line.split())
+        list1.append(num1)
+        list2.append(num2)
+
+    return list1, list2
 
 
 def part1(data=None):
-    result = None
+    a, b = parse_data(data)
+    a = list(sorted(a))
+    b = list(sorted(b))
+    result = 0
+    for a, b in zip(a, b):
+        result += abs(a - b)
     print(result)
     return result
 
 
 def part2(data=None):
-    result = None
+    a, b = parse_data(data)
+    result = 0
+    for x in a:
+        result += x * len([y for y in b if y == x])
     print(result)
     return result
 
 
 # %%
-if tester(part1, puz.examples[0]):
-    resa = part1(puz.input_data)
-    print(f"solution: {resa}")
-    puz.answer_a = resa
+print("answer:", puz.examples[0].answer_a)
+# resa = part1(puz.input_data)
+# print(f"solution: {resa}")
+# puz.answer_a = resa
 
 # %%
-if tester(part2, puz.examples[1]):
-    resb = part2(puz.input_data)
-    print(f"solution: {resb}")
-    puz.answer_b = resb
+print("answer:", puz.examples[0].answer_b)
+# resb = part2(puz.input_data)
+# print(f"solution: {resb}")
+# puz.answer_b = resb
+
+# %%
